bec308b8944eafa1ee82795bf2995f7ebffa673c
Chris D <faithoflifedev@gmail.com>
2022-08-11 19:30:27 -0400

    global var

--- Formula/onvif.rb
+++ Formula/onvif.rb
@@ -1,10 +1,13 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+$onvif_version = 2.0.10
+
 class Onvif < Formula
 	desc "Command line ONVIF Client protocol Profile S (Live Streaming)."
 	homepage "https://github.com/faithoflifedev/easy_onvif"
-	url "https://github.com/faithoflifedev/easy_onvif/archive/refs/tags/v2.0.10.tar.gz"
+	url "https://github.com/faithoflifedev/easy_onvif/archive/refs/tags/v#$onvif_version.tar.gz"
 	sha256 "59fce2f48604adfdc19a70124f3fcf54a574aa89957358c3ef9819334ae76a8a"
 	license "MIT"
   
@@ -14,12 +17,13 @@ class Onvif < Formula
   
 	def install
 		system "dart", "pub", "get"
+		system "dart", "pub", "cache", "add", "easy_onvif", "--version", "#$onvif_version"
 		system "dart", "compile", "exe", "bin/onvif.dart", "-o", "onvif"
 		bin.install "onvif"
 	  end
   
 	  test do
-		assert_match "easy_onvif v2.0.10", shell_output("#{bin}/onvif version")
+		assert_match "easy_onvif v#$onvif_version", shell_output("#{bin}/onvif version")
 	  end
   end
   
