06711d0ec15dd3015a2ef3892eaef6fa5ab9251f
Chris D <faithoflifedev@gmail.com>
2022-08-14 15:48:54 -0400

    2.0.11

--- .tmpl/onvif.tmpl.rb
+++ .tmpl/onvif.tmpl.rb
@@ -1,10 +1,10 @@
-$version = "2.0.11"
+$version = "__VERSION__"
 
 class Onvif < Formula
 	desc "Command line ONVIF Client protocol Profile S (Live Streaming)."
 	homepage "https://github.com/faithoflifedev/easy_onvif"
-	url "https://github.com/faithoflifedev/easy_onvif/archive/refs/tags/v2.0.11.tar.gz"
-	sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+	url "__ARCHIVE__"
+	sha256 "__CHECKSUM__"
 	license "MIT"
 
 	depends_on "dart-lang/dart/dart" => :build
---
+++ commit.sh
@@ -0,0 +1,45 @@
+#! /usr/local/bin/bash
+
+# derive the repo from the current one
+repo=$(git config --get remote.origin.url | sed -e 's|https://github.com/||' | sed -e 's|homebrew-||' | sed -e 's|\.git||')
+
+# get the version from the config file in the main repo 
+version=$(wget -q https://raw.githubusercontent.com/${repo}/main/tool/config.yaml -O - | yq '.version' )
+
+last_version=""
+
+# get the previous commit version
+if test -f ".last_version"; then
+	last_version=$(cat .version)
+fi
+
+# confirm this is a new commit
+if [ "$last_version" = "$version" ]; then
+	echo "The config file has not been updated. Execution complete."
+
+	exit
+fi
+
+archive="https://github.com/${repo}/archive/refs/tags/v${version}.tar.gz"
+
+target="Formula/$(ls Formula)"
+
+template=$(echo ${target} | sed -e 's/Formula/.tmpl/' | sed -e 's/\.rb/\.tmpl\.rb/')
+
+checksum=$(curl -s ${archive} | sha256sum | awk '{ print $1 }')
+
+rm -rf ${target}
+
+cat "${template}" \
+	| sed -e "s/__VERSION__/${version}/" \
+	| sed -e "s|__ARCHIVE__|${archive}|" \
+	| sed -e "s/__CHECKSUM__/${checksum}/" \
+> ${target}
+
+git add .
+
+git commit -m "${version}"
+
+git push
+
+echo "${version}" > .last_version
