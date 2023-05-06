2bba1a8045a1836afa3319eedafbb758a26306fc
Chris D <faithoflifedev@gmail.com>
2023-05-06 18:43:18 -0400

    v2.1.2+8

--- Formula/onvif.rb
+++ Formula/onvif.rb
@@ -1,8 +1,8 @@
 class Onvif < Formula
   desc "A pure Dart library designed primarily for command line automation of Onvif compatible devices, but can be used anywhere Dart is used."
   homepage "https://github.com/faithoflifedev/easy_onvif"
-  url "https://api.github.com/repos/faithoflifedev/easy_onvif/tarball/v2.1.2+7"
-  sha256 "5a213e2e932738d05e08b256f547e789d5c62bf52fb2c5f82fa7118072827a54"
+  url "https://api.github.com/repos/faithoflifedev/easy_onvif/tarball/v2.1.2+8"
+  sha256 "abd7c0c530f581f046bcaeb5033bd72e2949731db84aa5850bf3bfdfb0441619"
   license "MIT"
   
   depends_on "dart-lang/dart/dart" => :build
@@ -14,6 +14,6 @@ class Onvif < Formula
   end
   
   test do
-    assert_match "obs_websocket v2.1.2+7", shell_output("bin/onvif version")
+    assert_match "obs_websocket v2.1.2+8", shell_output("bin/onvif version")
   end
 end
